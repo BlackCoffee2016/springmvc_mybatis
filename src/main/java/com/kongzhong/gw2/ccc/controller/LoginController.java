8c6b17d50d8136002b1091206aedc72b259248d2
yangcan <yangcan@sh-yangcan.hongdian.cn>
2016-03-15 16:02:31 +0800

    spring mvc+mybatis模板代码

---
+++ src/main/java/com/kongzhong/gw2/ccc/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.kongzhong.gw2.ccc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/login")
+	public ModelAndView login() throws Exception {
+		throw null;
+	}
+
+}
